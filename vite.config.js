0469f2666be27ef1bb62fbc0b22990171cd4a9b3
Hannah Gray <hannah7gray@gmail.com>
2026-05-06 11:10:26 -0600

    Add Physics 1 Final Boss Study Game

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/physics-1-final-boss-study-game/"
+});
